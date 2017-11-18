63c43a3a6198bf39516a4d893339021bb837e479
André Lustosa Cabral de Paula Motta <andre.motta@dcc.ufmg.br>
2017-11-18 16:41:38 -0200

    Commit

--- Modulo 1/Aula 00/Aula 00/main.c
+++ Modulo 1/Aula 00/Aula 00/main.c
@@ -1,1 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(){
+
+    int a;
+    int b;
+    int solucao;
+    double divis;
+    char oper;
+
+    oper = 'o';
+
+    printf("Digite o primeiro numero\n");
+    scanf("%d", &a);
+    printf("Digite o segundo numero\n");
+    scanf("%d", &b);
+    printf("Selecione a operacao\n");
+    printf("a para adicao\n");
+    printf("s para subtracao\n");
+    printf("m para multiplicao\n");
+    printf("d para divisao\n");
+    scanf(" %c", &oper);
+
+    if (oper == 'a')
+    {
+    solucao = a + b;
+    printf("%d = %d + %d\n", solucao, a, b);
+    }
+
+    if (oper == 's')
+    {
+    solucao = a - b;
+    printf("%d = %d - %d\n", solucao, a, b);
+    }
+
+    if (oper == 'm')
+    {
+    solucao = a * b;
+    printf("%d = %d * %d\n", solucao, a, b);
+    }
+
+    if (oper == 'd')
+    {
+    divis = (double)a / (double)b;
+    printf("%u= %d / %d\n", divis, a, b);
+    }
+}
